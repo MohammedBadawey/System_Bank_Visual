b46405268359427b18f135058d92f9139c7a0a81
Mohammed Badawey <MohammedMBadawey11@gmail.com>
2024-08-21 14:36:24 +0300

    Update System_Bank_Visual.cpp

--- System_Bank_Visual/System_Bank_Visual.cpp
+++ System_Bank_Visual/System_Bank_Visual.cpp
@@ -18,9 +18,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello";
-    //Admin* admin = Admin::getInstance();
-   // Functions::OpenSystem();
+    //cout << "Hello";
+    Admin* admin = Admin::getInstance();
+    Functions::OpenSystem();
     return 0;
 
 
