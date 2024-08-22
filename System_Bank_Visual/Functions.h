b668ea879d94918c3b57dc3581e591902a5fc273
MohammedBadawey <MohammedMBadawey11@gmail.com>
2024-08-22 21:07:55 +0300

    V1.2.2

--- System_Bank_Visual/Functions.h
+++ System_Bank_Visual/Functions.h
@@ -376,4 +376,61 @@ public:
         showBalance->checkBalance();
     }
 
+
+
+
+
+
+
+    class Parser
+    {
+    public:
+        static void saveClient(Client* c) {
+            ofstream ofS("Clients.txt", ios::app);
+            if (ofS) {
+                ofS << c->getId() << "|" << c->getName() << "|" << c->getPassword() << "|" << c->getBalance() << endl;
+            }
+            else {
+                cout << "Error opening Clients.txt for writing!" << endl;
+            }
+        }
+
+        static void readClient() {
+            ifstream ifS;
+            ifS.open("clientData.txt");
+            string line;
+            string myClient[4];
+
+            while (getline(ifS, line)) {
+                int pos = 0;
+                string split;
+                int index = 0;
+                while (pos != -1) {
+                    pos = line.find("|");
+                    split = line.substr(0, pos);
+                    line = line.substr(pos + 1);
+                    myClient[index] = split;
+                    index++;
+                };
+                int id = stoi(myClient[0]);
+                string name = myClient[1];
+                string password = myClient[2];
+                int balance = stoi(myClient[3]);
+                vector <Client*> readClient;
+                Client* newClient = new Client(name, id, password, balance);
+                clientList.push_back(newClient);
+            };
+
+
+        }
+
+
+
+
+    };
+
+
+
+
+
 };
--- System_Bank_Visual/Parser.h
+++ System_Bank_Visual/Parser.h
@@ -3,59 +3,57 @@
 #include <string>
 #include <cmath>
 #include <vector>
-#include "fstream"
-#include "Person.h"
+#include <fstream>
 #include "Client.h"
-#include "Employee.h"
-#include "Admin.h"
-#include "Validation.h"
 
 
 using namespace std;
 
-class Parser
-{
-public:
-	static void saveClient (Client* c){
-		ofstream ofS;
-		ofS.open("clientData.txt",ios::app);
-		if (ofS.is_open()) {
-			ofS << c->getId() << "|" << c->getName() << "|" << c->getPassword() << "|" << c->getBalance() << endl;
-			ofS.close();
-		}
-		else { cout << "Error opening file for writing!" << endl;}
-	}
-
-	static void readClient() {
-		ifstream ifS;
-		ifS.open("clientData.txt");
-		string line;
-		string myClient[4];
-
-		while (getline(ifS, line)) {
-			int pos = 0;
-			string split;
-			int index = 0;
-			while (pos != -1) {
-				pos = line.find("|");
-				split = line.substr(0, pos);
-				line = line.substr(pos + 1);
-				myClient[index] = split;
-				index++;
-			};
-			int id = stoi(myClient[0]);
-			string name = myClient[1];
-			string password = myClient[2];
-			int balance = stoi(myClient[3]);
-			Client* newClient = new Client(name, id, password, balance);
-			newClient->Display();
-		};
-
-		
-	}
-
-
-
-
-};
+//class Functions;
+//class Parser
+//{
+//public:
+//	static void saveClient(Client* c) {
+//		ofstream ofS("Clients.txt", ios::app);
+//		if (ofS) {
+//			ofS << c->getId() << "|" << c->getName() << "|" << c->getPassword() << "|" << c->getBalance() << endl;
+//		}
+//		else {
+//			cout << "Error opening Clients.txt for writing!" << endl;
+//		}
+//	}
+//
+//	static void readClient() {
+//		ifstream ifS;
+//		ifS.open("clientData.txt");
+//		string line;
+//		string myClient[4];
+//
+//		while (getline(ifS, line)) {
+//			int pos = 0;
+//			string split;
+//			int index = 0;
+//			while (pos != -1) {
+//				pos = line.find("|");
+//				split = line.substr(0, pos);
+//				line = line.substr(pos + 1);
+//				myClient[index] = split;
+//				index++;
+//			};
+//			int id = stoi(myClient[0]);
+//			string name = myClient[1];
+//			string password = myClient[2];
+//			int balance = stoi(myClient[3]);
+//			vector <Client*> readClient;
+//			Client* newClient = new Client(name, id, password, balance);
+//			readClient.push_back(newClient);
+//		};
+//
+//		
+//	}
+//
+//
+//
+//
+//};
 
